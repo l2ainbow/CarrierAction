13895f2d85e5613fc1d0523cdcce804c39564fbf
Yu Iijima <y.iijima@nddhq.co.jp>
2017-10-14 18:22:38 +0900

    モータドライバクラスの追加（CarrierActionからは未参照）

---
+++ MotorDriver.cpp
@@ -0,0 +1,78 @@
+/**
+  モータドライバのクラス
+  author: Yu Iijima
+ **/
+
+#include "MotorDriver.h"
+#include <Arduino.h>
+#include <MsTimer2.h>
+
+// コンストラクタ
+// pin1: モータドライバの入力端子1と接続しているArduinoのPIN番号
+// pin2: モータドライバの入力端子2と接続しているArduinoのPIN番号
+MotorDriver::MotorDriver(int pin1, int pin2)
+:
+  pin1(pin1),
+  pin2(pin2),
+  pwm(0)
+{
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+}
+
+// モータを回転する
+// pwm: モータにかける電圧のPWM (-100(後進) ~ 100(前進))
+void MotorDriver::rotate(int pwm) {
+  int lastPWM = this->pwm;
+  this->pwm = pwm;
+
+  boolean isAhead = (pwm > 0);
+  boolean isBreak = (pwm == 0);
+
+  int val = (int)(abs(pwm) / 100.0 * 255);
+
+  //Serial.print(val);
+  //Serial.print(",");
+  //Serial.print(isAhead);
+  //Serial.print(",");
+  //Serial.print(isBreak);
+  //Serial.print(",R");
+
+  if (lastPWM == 0 && pwm != 0) {
+    val = 255;
+    //MsTimer2::set(BOOST_TIME, rotateAgain);
+    //MsTimer2::start();
+  }
+  if (isBreak) {
+    digitalWrite(pin1, HIGH);
+    digitalWrite(pin2, HIGH);
+  }
+  else if (isAhead) {
+    analogWrite(pin1, 0);
+    analogWrite(pin2, val);
+  }
+  else {
+    analogWrite(pin1, val);
+    analogWrite(pin2, 0);
+  }
+}
+
+// 右モータの再回転
+void MotorDriver::rotateAgain() {
+  //MsTimer2::stop();
+  int val = (int)(abs(this->pwm) / 100.0 * 255);
+
+  if (this->pwm == 0) {
+    digitalWrite(pin1, HIGH);
+    digitalWrite(pin2, HIGH);
+  }
+  else if (this->pwm > 0) {
+    analogWrite(pin1, 0);
+    analogWrite(pin2, val);
+  }
+  else {
+    analogWrite(pin1, val);
+    analogWrite(pin2, 0);
+  }
+}
+
---
+++ MotorDriver.h
@@ -0,0 +1,22 @@
+#ifndef MOTORDRIVER_H
+#define MOTORDRIVER_H
+
+#include <Arduino.h>
+
+class MotorDriver
+{
+  // モータのブースト時間（停止〜加速）[ms]
+  static const int BOOST_TIME = 50;
+  public:
+    MotorDriver(int pin1, int pin2);
+    void rotate(int pwm);
+  private:
+    void rotateAgain();
+    int pin1;
+    int pin2;
+    int pwm;
+};
+
+#endif
+
+

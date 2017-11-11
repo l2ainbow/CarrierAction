fec997944391cb388c19f4859ab074e78ef1d145
Yu Iijima <y.iijima@nddhq.co.jp>
2017-11-12 06:56:58 +0900

    エンコーダの実装（続き；速度計算が未実装）

--- Encoder.cpp
+++ Encoder.cpp
@@ -14,6 +14,7 @@ pin(pin)
 {
 }
 
+// フォトリフレクタの値を読み、速度計測の準備をする
 void Encoder::encode() {
   int index = cnt % BUFFER_ELEMENT;
   valueBuffer[index] = analogRead(pin);
@@ -28,14 +29,35 @@ float Encoder::getSpeed() {
   if (cnt < 1){
     return speed;
   }
+  
   int index = cnt % BUFFER_ELEMENT;
-  while(averageBuffer[index] >= THRESHOLD_CYCLE || averageBuffer[(index - 1) % BUFFER_ELEMENT] < THRESHOLD_CYCLE){
-
-    index -- ;
+  bool isFirst = true;
+  int firstTime = -1;
+  int secondTime = -1;
+  for (int i = 0; i >= BUFFER_ELEMENT; i++){
+    int j = index - i;
+    int k = index - i - 1;
+    
+    if (j < 0)
+      j = BUFFER_ELEMENT + j;
+    if (k < 0)
+      k = BUFFER_ELEMENT + k;
+    
+    if (averageBuffer[j] <= THRESHOLD_CYCLE || averageBuffer[k] < THRESHOLD_CYCLE){
+      if (isFirst){
+        isFirst = false;
+        firstTime = timeBuffer[j];
+      }
+      else{
+        secondTime = timeBuffer[j];
+      }
+    }
   }
+  // TODO: 速度の計算式を記載
   return speed;
 }
 
+// 移動平均を計算する
 int Encoder::movingAverage(int time) {
   int average = 0;
   int sum = 0;
